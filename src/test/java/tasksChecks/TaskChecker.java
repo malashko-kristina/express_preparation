23be4c97a70faba0590ee920d187de052e1b4acf
Kristina Malashko <kristina.malashko@pinch.nl>
2024-12-08 19:56:22 +0100

    Add homework 4 (Data Structure)

--- src/test/java/tasksChecks/TaskChecker.java
+++ src/test/java/tasksChecks/TaskChecker.java
@@ -1,10 +1,25 @@
 package tasksChecks;
 
-import javaTasks.FirstHomeWork;
+import javaTasks.DataStructure;
+import javaTasks.UpdateString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TaskChecker {
     public static void main(String[] args) {
+        int[] numbers = {17, 8, 9, 2, 4, 7, 4, 4, 2};
+//        System.out.println(DataStructure.sumArray(numbers));
+//        System.out.println(DataStructure.reverseString("Hello pretty"));
+
+//        List<Integer> numbers = new ArrayList<>(List.of(4, 8, 0, 3, 9, 11));
+//        System.out.println(DataStructure.cleanList(numbers));
+
+//        System.out.println(DataStructure.returnSet(numbers));
+//        System.out.println(UpdateString.removeDoubledCharacters("k"));
+//        System.out.println(DataStructure.resultMap("Kikaa"));
+        // Создаем односвязный список: 1 -> 2 -> 3 -> 4 -> null
 
     }
 }
